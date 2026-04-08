5f47cdee5b1843c7f2f0da8c2c25aa1c76379522
sophyamirza <YOUR_EMAIL@example.com>
2026-04-08 09:29:34 -0700

    remove tldr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import NavBar from "@/components/NavBar";
 import PageTransition from "@/components/PageTransition";
 import CursorHalo from "@/components/CursorHalo";
